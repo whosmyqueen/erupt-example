150c7139462ef2d87a828293da7a20c9f5c2e3e0
郑志辉 <644049260@qq.com>
2021-07-20 15:45:36 +0800

    init commit

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -16,4 +16,45 @@ window.eruptSiteConfig = {
     logoText: "erupt",
     //是否开启路由复用
     routerReuse: true,
+    //自定义导航栏按钮，配置后将会出现在页面右上角
+    r_tools: [{
+        text: "自定义功能按钮",
+        icon: "fa-eercast",
+        mobileHidden: true,
+        click: function (event) {
+            alert("Function button");
+        }
+    }]
 };
+
+//路由回调函数
+window.eruptRouterEvent = {
+    //key表示要监听的路由切换地址，为url hash地址最后一段
+    //例如：http://www.erupt.xyz:9999/#/build/table/demo中demo为回调key
+    demo: {
+        //路由载入事件
+        load: function (e) {
+
+        },
+        //路由退出事件
+        unload: function (e) {
+
+        }
+    },
+    //$ 为全路径通配符，在任何路由切换时都会执行load与unload事件
+    $: {
+        load: function (e) {
+
+        },
+        unload: function (e) {
+        }
+    }
+};
+
+//erupt生命周期函数
+window.eruptEvent = {
+    //页面加载完成后回调
+    startup: function () {
+
+    }
+}
